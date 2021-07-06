8ca3306dcc984a4133f3ab4d9ef4fadec6fc36d3
Mark <markjsapp@gmail.com>
2021-07-06 19:13:11 -0400

    Resolving deployment issues
    Commenting out the map import to make AWS happy.

--- src/App.js
+++ src/App.js
@@ -1,77 +1,97 @@
-import './App.css';
-import React from 'react';
-import Footer from './components/footer';
-import {Card, ListGroup} from 'react-bootstrap';
-import Maps from './components/map';
+import "./App.css";
+import React from "react";
+import Footer from "./components/footer";
+import { Card, ListGroup } from "react-bootstrap";
+//import Maps from "./components/map";
 
-// Temp data 
+// Temp data
 const restaurants = [
-  {name: 'Chik-Fil-A'},
-  {name: 'Waffle House'},
-  {name: 'McDonalds'},
-  {name: 'Cafe Zuppina'},
-  {name: 'Firehouse'},
-  {name: 'IHOP'},
-  {name: 'Dunkin'},
-  {name: 'Born & Bread'},
-  {name: 'Hole in One'},
-  {name: 'Olive Garden'}
-  ]
+  { name: "Chik-Fil-A" },
+  { name: "Waffle House" },
+  { name: "McDonalds" },
+  { name: "Cafe Zuppina" },
+  { name: "Firehouse" },
+  { name: "IHOP" },
+  { name: "Dunkin" },
+  { name: "Born & Bread" },
+  { name: "Hole in One" },
+  { name: "Olive Garden" },
+];
 
-// Calculating a random index 
+// Calculating a random index
 const randIndex = Math.floor(Math.random() * restaurants.length);
 
-// Main 
+// Main
 class App extends React.Component {
-
   // Default state doesn't show a restaurant
   state = {
     showMessage: false,
-    place: restaurants[randIndex].name
-  }
+    place: restaurants[randIndex].name,
+  };
 
   // Clicking the button enables the restaurant
   onButtonClickHandler = () => {
-   this.setState(prevState => {
-     return {
-      showMessage:true, 
-      place: restaurants[Math.floor(Math.random() * restaurants.length)].name 
-    }
-   })
+    this.setState((prevState) => {
+      return {
+        showMessage: true,
+        place: restaurants[Math.floor(Math.random() * restaurants.length)].name,
+      };
+    });
   };
 
-  render(){
+  render() {
     return (
       <div className="App">
         <h1>Where The Fuck Should We Eat?</h1>
-        {this.state.showMessage && 
-          <p>{this.state.place}
-            <Card style={{ 
-              textAlign: "center", 
-              paddingLeft: "115px", 
-              paddingRight: "115px",
-              }}>
-              <Card.Header style={{ 
-                fontSize: "30px",
-                color: "grey"
-              }}>Hours</Card.Header>
+        {this.state.showMessage && (
+          <p>
+            {this.state.place}
+            <Card
+              style={{
+                textAlign: "center",
+                paddingLeft: "115px",
+                paddingRight: "115px",
+              }}
+            >
+              <Card.Header
+                style={{
+                  fontSize: "30px",
+                  color: "grey",
+                }}
+              >
+                Hours
+              </Card.Header>
               <ListGroup variant="flush">
-                <ListGroup.Item style={{ fontSize: "30px", color: "grey"}}>Monday: 8:00am - 10:00pm </ListGroup.Item>
-                <ListGroup.Item style={{ fontSize: "30px", color: "grey"}}>Tuesday: 8:00am - 10:00pm </ListGroup.Item>
-                <ListGroup.Item style={{ fontSize: "30px", color: "grey"}}>Wednesday: 8:00am - 10:00pm </ListGroup.Item>
-                <ListGroup.Item style={{ fontSize: "30px", color: "grey"}}>Thursday: 8:00am - 10:00pm</ListGroup.Item>
-                <ListGroup.Item style={{ fontSize: "30px", color: "grey"}}>Friday: 8:00am - 10:00pm</ListGroup.Item>
+                <ListGroup.Item style={{ fontSize: "30px", color: "grey" }}>
+                  Monday: 8:00am - 10:00pm{" "}
+                </ListGroup.Item>
+                <ListGroup.Item style={{ fontSize: "30px", color: "grey" }}>
+                  Tuesday: 8:00am - 10:00pm{" "}
+                </ListGroup.Item>
+                <ListGroup.Item style={{ fontSize: "30px", color: "grey" }}>
+                  Wednesday: 8:00am - 10:00pm{" "}
+                </ListGroup.Item>
+                <ListGroup.Item style={{ fontSize: "30px", color: "grey" }}>
+                  Thursday: 8:00am - 10:00pm
+                </ListGroup.Item>
+                <ListGroup.Item style={{ fontSize: "30px", color: "grey" }}>
+                  Friday: 8:00am - 10:00pm
+                </ListGroup.Item>
               </ListGroup>
             </Card>
           </p>
-        }
-        <button type="button" onClick={this.onButtonClickHandler} style={{margintop: "50px"}}>
+        )}
+        <button
+          type="button"
+          onClick={this.onButtonClickHandler}
+          style={{ margintop: "50px" }}
+        >
           PICK SOMETHING FOR ME DAMMIT
         </button>
         <Footer>
           <span>
-            <a href="https://www.buymeacoffee.com/markjsapp">Donate </a> | 
-            <a href="https://github.com/markjsapp">Github</a> 
+            <a href="https://www.buymeacoffee.com/markjsapp">Donate </a> |
+            <a href="https://github.com/markjsapp">Github</a>
           </span>
         </Footer>
       </div>
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,33 +1,31 @@
 import React from "react";
 
 var style = {
-    backgroundColor: "#F8F8F8",
-    borderTop: "1px solid #E7E7E7",
-    textAlign: "center",
-    padding: "4px",
-    position: "fixed",
-    left: "0",
-    bottom: "0",
-    height: "50px",
-    width: "100%",
-}
+  backgroundColor: "#F8F8F8",
+  borderTop: "1px solid #E7E7E7",
+  textAlign: "center",
+  padding: "4px",
+  position: "fixed",
+  left: "0",
+  bottom: "0",
+  height: "50px",
+  width: "100%",
+};
 
 var phantom = {
-  display: 'block',
-  padding: '20px',
-  height: '60px',
-  width: '95%',
-}
+  display: "block",
+  padding: "20px",
+  height: "60px",
+  width: "95%",
+};
 
 function Footer({ children }) {
-    return (
-        <div>
-            <div style={phantom} />
-            <div style={style}>
-                { children }
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <div style={phantom} />
+      <div style={style}>{children}</div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
